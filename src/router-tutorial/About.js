97cbd87811f5fa2f37208a59c150478996218620
Rilyaus <rilyaus.gd@gmail.com>
2021-08-01 12:25:45 +0900

    feat : router-tutorial

---
+++ src/router-tutorial/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import qs from "qs";
+
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const showDetail = query.detail === "true";
+
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>라우트 기초 실습 프로젝트!</p>
+      {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
+    </div>
+  );
+};
+
+export default About;
