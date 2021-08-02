6868ebb24d3504333c110e15f947f41562d9269e
Rilyaus <rilyaus.gd@gmail.com>
2021-08-02 23:41:11 +0900

    feat: news-viewer App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./router-tutorial/App.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './news-viewer/App.js';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
